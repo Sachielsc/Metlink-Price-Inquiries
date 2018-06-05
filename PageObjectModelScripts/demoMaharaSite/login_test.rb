7d7884e07d170369769e4ee57fcef5eb8a57a1b4
Charles Shu <cshu@planittesting.com>
2018-06-05 13:43:22 +1200

    Finish editing demoMaharaSite files TODO: wait.until

--- PageObjectModelScripts/demoMaharaSite/login_test.rb
+++ PageObjectModelScripts/demoMaharaSite/login_test.rb
@@ -2,7 +2,7 @@ require_relative 'site_element.rb'
 
 #define new browser (or driver)
 browser=SiteElement.new("http://demo.mahara.org")
-wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+
 
 #define local variables
 username = "chris"
@@ -16,12 +16,5 @@ browser.login_password.send_keys(password)
 
 #click on submit button
 browser.submit_button.click
-
-#wait until the Logout link displays, timeout in 10 seconds
-wait.until {browser.logout_link }
-
-#if logout link is displayed
-isLogoutLinkDisplayed=browser.logout_link.displayed?
-puts isLogoutLinkDisplayed
 
 browser.close_browser
--- PageObjectModelScripts/demoMaharaSite/site_element.rb
+++ PageObjectModelScripts/demoMaharaSite/site_element.rb
@@ -5,20 +5,31 @@ class SiteElement
     @driver=Selenium::WebDriver.for :firefox
     @driver.manage.window.maximize
     @driver.navigate.to url
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
   end
+
   def login_username()
     @driver.find_element(:id,'login_login_username')
   end
+
   def login_password()
     @driver.find_element(:id,'login_login_password')
   end
+
   def submit_button()
     @driver.find_element(:id,'login_submit')
   end
+
   def logout_link()
     @driver.find_element(:link_text,'Logout')
   end
+
   def close_browser()
     @driver.quit
   end
+
+  def chevron_down_button()
+    @driver.find_element(:css, "button.user-toggle.navbar-toggle > span.icon.icon-chevron-down.collapsed")
+  end
+
 end

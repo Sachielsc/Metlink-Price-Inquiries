49a6c4dc8da5551bbb774f7b20ae40b37660458f
Charles Shu <cshu@planittesting.com>
2018-06-08 11:31:19 +1200

    Working version 4.0 (Official) 1. Now the script can extract the prices

--- RubyRSpecScripts/RSpecMetlink/pageObjects/metlink_home_page.rb
+++ RubyRSpecScripts/RSpecMetlink/pageObjects/metlink_home_page.rb
@@ -1,23 +1,9 @@
 require 'selenium-webdriver'
+include RSpec::Matchers
 
 class MetlinkHomePage
 
-  def initialize(url)
-    chrome_driver_path = File.join(File.dirname(__FILE__), '../../../drivers/chromedriver.exe')
-    Selenium::WebDriver::Chrome.driver_path = chrome_driver_path
-
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--disable-translate')
-    options.add_argument('--disable-infobars')
-
-    @driver = Selenium::WebDriver.for :chrome, options: options
-    @driver.manage.window.maximize
-    @driver.navigate.to url
-    @wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-  end
-
+  #Web elements
   def sign_in_link
     @driver.find_element(:css, "div.navbar-right-holder li>a[title='Sign In']")
   end
@@ -38,12 +24,64 @@ class MetlinkHomePage
     @driver.find_element(:id, "CustomMemberLoginForm_LoginForm_action_dologin")
   end
 
-  # def customer_info
-  #   @driver.find_element(:css, "input[name='Email']")
-  # end
+  def from_location_input_field
+    @driver.find_element(:id, "JourneyPlannerForm_JourneyPlannerForm_From")
+  end
+
+  def to_location_input_field
+    @driver.find_element(:id, "JourneyPlannerForm_JourneyPlannerForm_To")
+  end
+
+  def show_my_journey_button
+    @driver.find_element(:id, "JourneyPlannerForm_JourneyPlannerForm_action_doForm")
+  end
+
+  def fares_list
+    @driver.find_element(:css, "div.fares__list-wrapper")
+  end
+
+  def adult_cash
+    @driver.find_elements(:css, "div.fares__right>span.fares__price")[0]
+  end
+
+  def child_cash
+    @driver.find_elements(:css, "div.fares__right>span.fares__price")[1]
+  end
+
+  #Web page operations
+  def initialize(url)
+    chrome_driver_path = File.join(File.dirname(__FILE__), '../../../drivers/chromedriver.exe')
+    Selenium::WebDriver::Chrome.driver_path = chrome_driver_path
+
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--disable-translate')
+    options.add_argument('--disable-infobars')
+
+    @driver = Selenium::WebDriver.for :chrome, options: options
+    @driver.manage.window.maximize
+    @driver.navigate.to url
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 15) # seconds
+  end
 
   def close_browser
     log_out_link.click
     @driver.quit
+  end
+
+  def check_price(from_location, to_location)
+    from_location_input_field.send_keys from_location
+    to_location_input_field.send_keys to_location
+
+    #assertion
+    #expect(from_location_input_field.text).to eq from_location
+    #expect(to_location_input_field.text).to eq to_location
+
+    show_my_journey_button.click
+  end
+
+  def tell_price
+    puts "\nAdult Cash: " + adult_cash.text + "\nChild Cash: " + child_cash.text if @wait.until {fares_list.displayed?}
   end
 end
--- RubyRSpecScripts/RSpecMetlink/spec/metlink_spec.rb
+++ RubyRSpecScripts/RSpecMetlink/spec/metlink_spec.rb
@@ -4,26 +4,33 @@ require_relative '../pageObjects/metlink_home_page'
 
 describe MetlinkHomePage do
 
+  #Addresses
+  library_address = "65 Victoria St, Wellington, 6011"
+  museum_address = "55 Cable St, Te Aro, Wellington 6011"
+  target_address = "104 Moorefield Road, Johnsonville, Wellington, New Zealand"
+
   before(:each) do
-    @email = "newzealand1126@gmail.com"
-    @password = "Scsgdtcy3"
+    email = "newzealand1126@gmail.com"
+    password = "Scsgdtcy3"
     home_page_url = "https://www.metlink.org.nz"
 
     #Go to the home page
-    @browser=MetlinkHomePage.new(home_page_url)
+    @browser = MetlinkHomePage.new(home_page_url)
+
+    #Log in
+    @browser.sign_in_link.click
+    @browser.email_input_field.send_keys email
+    @browser.password_input_field.send_keys password
+    @browser.sign_in_button.click
   end
 
   after(:each) do
+    #Logout and close the browser
     @browser.close_browser
   end
 
-  it "should display the path and price" do
-    @browser.sign_in_link.click
-    @browser.email_input_field.send_keys @email
-    @browser.password_input_field.send_keys @password
-    @browser.sign_in_button.click
-
-    #assertion
-    #expect(@browser.customer_info.text).to eq @email
+  it "should display the price from target to Museum of New Zealand" do
+    @browser.check_price(target_address, museum_address)
+    @browser.tell_price
   end
 end
